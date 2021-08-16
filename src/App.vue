f367ded59a754cc9ff21fb8a228ed4c7b772e2fd
tamatsukuri01 <ysk0102tama@gmail.com>
2021-08-16 22:24:39 +0900

    8.16

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-title>
+        マイアドレス帳
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <SideNav/>
+
+    <v-main>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SideNav from './components/SideNav.vue'
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  data: () => ({
+    //
+  }),
+  components: {
+    SideNav
   }
-}
-</style>
+};
+</script>
